3f04c0f12bca8ebc05f353beafc3cf09be1264cc
saichander <saichander17@gmail.com>
2017-12-02 20:45:05 +0530

    activeadmin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope "v1/orders/", :module => "api/v1", :format => :json do
   	post 'create', to: 'orders#create'
