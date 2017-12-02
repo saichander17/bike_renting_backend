3dfbb31a999f9fb8ecb4946b991ad8b85b009d40
saichander <saichander17@gmail.com>
2017-12-02 20:48:47 +0530

    minor fix

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,7 +9,10 @@ class Order < ApplicationRecord
 
 	def assign_bike_number
 		available_bike_numbers = get_available_bike_number_ids_for_bike(self.bike_id,self.start_time,self.end_time,self.location_id)
-		return true if available_bike_numbers.length>0
+		if available_bike_numbers.length>0
+			self.bike_number_id = available_bike_numbers[0]
+			return true 
+		end
 		errors.add(:bike, "is not available for given time slot and location")
 		false
 	end
